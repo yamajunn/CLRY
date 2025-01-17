6aad6decdfe7c9edd06c7208f635354d9418f21e
yamajunn <amiton1115@gmail.com>
2025-01-17 09:08:00 +0900

    画像キャプチャ機能をImageGrabからpyautoguiに移行し、関数名を変更

--- view.py
+++ view.py
@@ -1,16 +1,13 @@
 import pywinctl as pwc
-from PIL import ImageGrab
+import pyautogui
 
-def capture_region(x, y, width, height, save_path):
-    region = (x, y, x + width, y + height)
-    screen = ImageGrab.grab(bbox=region)
-    screen.save(save_path)
+def capture_area(filename, x, y, width, height):
+    image = pyautogui.screenshot(region=(x, y, width, height))
+    image.save(filename)
 
 # print(pwc.getAllWindows())
 # print(pwc.getAllTitles())
 # print(pwc.getWindowsWithTitle('iPhoneミラーリング'))
 win = pwc.getWindowsWithTitle('iPhoneミラーリング')[0]
 win.activate()
-print(win.left+5, win.top+35, win.left+win.width-5, win.top+win.height-7)
-    
-capture_region(win.left+6, win.top+37, win.width-12, win.height-43, "region_screenshot.png")
+capture_area("region_screenshot.png", win.left+6, win.top+37, win.left+win.width-6, win.top+win.height-10)
